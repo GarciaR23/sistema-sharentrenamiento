647a5141475fa174e5b1256cfb1414c9eb64453b
Gerardo Ramos <jramos5191@gmail.com>
2026-04-30 09:27:39 -0500

    Rutas

--- frontend/src/app/pages/landing/components/hero/hero.component.ts
+++ frontend/src/app/pages/landing/components/hero/hero.component.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
   templateUrl: './hero.component.html',
+  imports: [CommonModule, RouterLink],
   styleUrls: ['./hero.component.css']
 })
-export class HeroComponent {}
+export class HeroComponent {
+  isMenuOpen = false;
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+}
