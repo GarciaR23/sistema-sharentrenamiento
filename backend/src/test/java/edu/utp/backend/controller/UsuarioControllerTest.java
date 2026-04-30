beb5509b09999b96b55ad02c901293f2dc3a295e
Gerardo Ramos <jramos5191@gmail.com>
2026-04-30 02:50:19 -0500

    Descripción de los cambios

--- backend/src/test/java/edu/utp/backend/controller/UsuarioControllerTest.java
+++ backend/src/test/java/edu/utp/backend/controller/UsuarioControllerTest.java
@@ -1,6 +1,7 @@
 package edu.utp.backend.controller;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
